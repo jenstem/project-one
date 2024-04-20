9c9a84d04dbbf68a5dedb9ee50874f41b920770e
Jen <jennifer.stemkowski@gmail.com>
2024-04-20 10:58:32 -0400

    Create canvas and add to window

--- paint.py
+++ paint.py
@@ -1,14 +1,31 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import sys
+
+
+class Canvas(QLabel):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.pixmap = QPixmap(600, 600)
+        self.pixmap.fill(Qt.GlobalColor.white)
+        self.setPixmap(self.pixmap)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUI()
 
     def initUI(self):
-        self.setMinimumSize(400, 400)
+        self.setMinimumSize(600, 600)
         self.setWindowTitle('Paint App')
+
+        canvas = Canvas()
+        self.setCentralWidget(canvas)
 
 
 app = QApplication(sys.argv)
